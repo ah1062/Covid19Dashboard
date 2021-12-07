52907a2fd503b22f6cc1ca24c1f276af899636b5
ah1062 <ah1062@exeter.ac.uk>
2021-12-07 15:52:02 +0000

    toml creation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Alex Houghton'
 author = 'Alex Houghton'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 # -- General configuration ---------------------------------------------------
 
