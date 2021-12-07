a3af571cb0423b9ea84a13fc60a255686e397c33
ah1062 <ah1062@exeter.ac.uk>
2021-12-07 19:07:30 +0000

    0.0.7 include_package_data
    Including Non Code Files in the Wheel Build

--- setup.py
+++ setup.py
@@ -5,14 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Covid19Dashboard_ah1062",
-    version="0.0.5",
+    version="0.0.7",
     author="Alex Houghton",
     author_email="ah1062@exeter.ac.uk",
     description="Covid-19 Data Presentation Dashboard, ECM1400 Programming Coursework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ah1062/Covid19Dashboard",
-    packages=setuptools.find_packages(),
+    packages=['Covid19Dashboard_ah1062'],
+    package_data={
+        'Covid19Dashboard_ah1062': [
+            "*.json",
+            "templates/*.*",
+            "templates/index_files/*.*",
+            "CSV Data Files/*.*"
+        ]
+    },
+    include_package_data=True,
     classifiers=[
 
     ],
